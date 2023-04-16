71b04bd7cd9b9108d7cc74c2abba5d697f8698d5
bellakrim <bellakrim2032@gmail.com>
2023-04-16 10:36:14 +0000

    optimize code in cpp09

--- day09/ex00/main.cpp
+++ day09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:11:47 by abellakr          #+#    #+#             */
-/*   Updated: 2023/04/13 06:42:46 by abellakr         ###   ########.fr       */
+/*   Updated: 2023/04/16 10:33:05 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int main(int ac , char **av)
             throw std::runtime_error("Error: could not open file.");
          else 
          {
-            (void)av;
             BitcoinExchange b;
             b.DataCollect(av[1]);
             // b.printDataBase(); // print data base
